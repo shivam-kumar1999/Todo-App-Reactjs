a7842d08a31c69358fedd7811ca12f9b8ecde480
shivam <shivamkumarhost@gmail.com>
2024-03-08 21:44:42 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+
+  return (
+
+    <div>
+      <header className='header'>Header Section</header>
+      <main className='main'>
+
+        <section className='task'></section>
+        <section className='task'></section>
+        <section className='task'></section>
+
+
+      </main>
+    </div>
+
+  )
+}
+
+export default App
